3f41ea19975ec55c3c3a92837648c33c0128c724
Prashant Deepake <deepakeprashant1@gmail.com>
2022-05-29 18:15:38 +0530

    ADDED : REVERSE NUMBER PROGRAM

--- src/LogicalMainClass.java
+++ src/LogicalMainClass.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class LogicalMainClass {
     public static final int FIBONACCI_SERIES = 1;
     public static final int PERFECT_NUMBER = 2;
+    public static final int PRIME_NUMBER = 3;
+    public static final int REVERSE_NUMBER = 4;
 
     public static void main(String[] args) {
         int choice;
@@ -12,16 +14,20 @@ public class LogicalMainClass {
         System.out.println("1] FIBONACCI SERIES PROGRAM");
         System.out.println("2] PERFECT NUMBER PROGRAM");
         System.out.println("3] PRIME NUMBER PROGRAM");
+        System.out.println("4] REVERSE NUMBER PROGRAM");
         choice = scan.nextInt();
         switch (choice) {
             case FIBONACCI_SERIES:
                 object.fibonacciSeries();
                 break;
-            case 2:
+            case PERFECT_NUMBER:
                 object.perfectNumber();
                 break;
-            case 3:
+            case PRIME_NUMBER:
                 object.primeNumber();
+                break;
+            case REVERSE_NUMBER:
+                object.reverseNumber();
                 break;
             default:
         }
--- src/LogicalProgram.java
+++ src/LogicalProgram.java
@@ -5,8 +5,8 @@ public class LogicalProgram {
     Scanner scan = new Scanner(System.in);
 
     public void fibonacciSeries() {
-        int firstValue=0;
-        int secondValue=1;
+        int firstValue = 0;
+        int secondValue = 1;
         System.out.println("Enter any number");
         number = scan.nextInt();
         System.out.print("Fibonacci Series =");
@@ -17,43 +17,58 @@ public class LogicalProgram {
             secondValue = temp;
         }
     }
-    public void perfectNumber(){
-        int sumAllDivisor=0;
-        int i=1;
+
+    public void perfectNumber() {
+        int sumAllDivisor = 0;
+        int i = 1;
         System.out.println("Enter any number");
         number = scan.nextInt();
         System.out.println("Perfect Number");
-        System.out.print(number+" = ");
-        while (i<=number/2){
-            if (number%i==0)
-            {
+        System.out.print(number + " = ");
+        while (i <= number / 2) {
+            if (number % i == 0) {
                 System.out.print(i);
-                sumAllDivisor+=i;
+                sumAllDivisor += i;
                 System.out.print(" + ");
             }
             i++;
         }
-        if (number==sumAllDivisor) {
+        if (number == sumAllDivisor) {
             System.out.println("\n" + number + " = " + sumAllDivisor);
-            System.out.println(number+" Is Perfect Number");
+            System.out.println(number + " Is Perfect Number");
             return;
         }
         System.out.println("\n" + number + " = " + sumAllDivisor);
-        System.out.println(number+" Is Not Perfect Number");
+        System.out.println(number + " Is Not Perfect Number");
     }
-    public void primeNumber(){
-        int primeNumber=0;
+
+    public void primeNumber() {
+        int primeNumber = 0;
         System.out.println("Enter any number");
         number = scan.nextInt();
-        for (int i = 0; i < number/2; i++) {
-            if (number%2==0){
-                primeNumber=1;
+        for (int i = 0; i < number / 2; i++) {
+            if (number % 2 == 0) {
+                primeNumber = 1;
                 break;
             }
         }
-        if (primeNumber==0)
-            System.out.println(number+" Is Prime Number");
+        if (primeNumber == 0)
+            System.out.println(number + " Is Prime Number");
         else
-            System.out.println(number+" Is Not Prime Number");
+            System.out.println(number + " Is Not Prime Number");
+    }
+
+    public void reverseNumber() {
+        int reverse = 0;
+        int originNumber=0;
+        System.out.println("Enter four digit any number");
+        number = scan.nextInt();
+        originNumber=number;
+        while (number != 0) {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
+        }
+        System.out.println(originNumber+" The reverse of the given number is: " + reverse);
     }
 }
